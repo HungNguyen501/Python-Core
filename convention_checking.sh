4736c84b4cb2d8969cbbe90bbc762dba007e7717
hungnd8 <hungnd8@vng.com.vn>
2024-04-23 17:33:01 +0700

    add git-ci for python core

--- convention_checking.sh
+++ convention_checking.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
-echo "Check convention ..."
-python3 -m flake8 && python3 -m pylint unittest_async hdfs_tree_paths data_hub
-echo "Unit tests ..."
-python3 -m pytest -vv --cov ./ --cov-report term-missing --cov-fail-under=100
+check_pep8 () {
+    echo "Check convention..."
+    python3 -m flake8 && python3 -m pylint unittest_async hdfs_tree_paths data_hub
+}
+run_unit_tests () {
+    echo "Run unit tests..."
+    python3 -m pytest -vv --cov ./ --cov-report term-missing --cov-fail-under=100
+}
+if [ -z "${1}" ]; then
+    check_pep8
+    run_unit_tests
+    exit 0
+fi
+JOB_NAME=${1}
+if [ ${JOB_NAME} == "PEP8" ]; then
+    check_pep8
+elif [ ${JOB_NAME} == "TEST" ]; then
+    run_unit_tests
+else
+    echo "Type wrong job name (PEP8 or TEST), please retry."
+    exit 1
+fi
+exit 0
