9156d992f4cce4db70f8118b5c1627cc16abbf7a
HungNguyenDinh <48648494+HungNguyen501@users.noreply.github.com>
2024-04-26 15:30:40 +0700

    Feature/update ci (#2)
    * move all codes to src folder
    
    * upload new ci flow
    
    * upload new ci flow
    
    * upload new ci flow
    
    * upload new ci flow
    
    * upload new ci flow
    
    * upload new ci flow
    
    * upload new ci flow
    
    * update ci flow
    
    * test ci flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update readme
    
    * update ci-flow
    
    * update ci-flow
    
    * update ci-flow
    
    * update readme
    
    ---------
    
    Co-authored-by: hungnd8 <hungnd8@vng.com.vn>

--- convention_checking.sh
+++ convention_checking.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
+validate_ref_name () {
+    ref_type=${1}
+    ref_name=${2}
+    if [ "${ref_type}" == "tag" ] ||
+        { 
+            [ "${ref_type}" == "branch" ] && 
+            [[ "${ref_name}" == "main"  || ${ref_name} == feature/* ]]
+        };
+    then
+        echo "Branch name is qualified: ${ref_name}"
+        exit 0
+    else
+        echo "Branch name is invalid: ${ref_name}. It should be started with [feature/]"
+        exit 1
+    fi
+}
 check_pep8 () {
     echo "Check convention..."
-    python3 -m flake8 . --show-source --statistics && python3 -m pylint unittest_async hdfs_tree_paths data_hub
+    python3 -m flake8 . --show-source --statistics && python3 -m pylint src
 }
 run_unit_tests () {
     echo "Run unit tests..."
-    python3 -m pytest -vv --cov ./ --cov-report term-missing --cov-fail-under=100
+    python3 -m pytest -vv --cov ./src --cov-report term-missing --cov-fail-under=100
 }
 if [ -z "${1}" ]; then
     check_pep8
     run_unit_tests
-fi
-JOB_NAME=${1}
-if [ ${JOB_NAME} == "PEP8" ]; then
+elif [ ${1} == "PEP8" ]; then
     check_pep8
-elif [ ${JOB_NAME} == "TEST" ]; then
+elif [ ${1} == "TEST" ]; then
     run_unit_tests
+elif [ ${1} == "REF" ]; then
+    validate_ref_name ${2} ${3}
 else
-    echo "Type wrong job name (PEP8 or TEST), please retry."
+    echo "Type wrong job name (PEP8, TEST or REF), please retry."
     exit 1
 fi
---
+++ src/git_python/git_ci.py
@@ -0,0 +1,66 @@
+"""CI scritps for GitHub"""
+import sys
+from typing import List, Dict
+
+import click
+from git import Repo
+
+
+def get_changed_files_of_commit(repo: Repo, back_head_to: int) -> List[Dict]:
+    """List changed files of a specific commit that behinds HEAD
+
+    Args:
+        repo(Repo): Git Repo Object
+        back_to_head(int): the number points to diffrent of positions between HEAD and commit
+
+    Return list changed files
+    """
+    list_commits = list(repo.iter_commits())
+    return list(list_commits[back_head_to].stats.files.keys())
+
+
+def skip_convention_checking(repo: Repo, commit_hash: str):
+    """Verify changed files of last commit to determine skip
+    to check convention or not.
+    If all changed files is in list of ignore pattern then exit program with success code
+    else then exit program with error code
+
+    Args:
+        repo(Repo): GitHub repo Object
+        commit_hash(str): commit SHA value or HEAD~1..HEAD
+    """
+
+    changed_files = list(repo.commit(commit_hash).stats.files.keys())
+    list_ignored_files = [".md", ".ipynb",]
+    for file in changed_files:
+        if not any(ig in file for ig in list_ignored_files):
+            sys.exit(0)
+    print("Skip pipelines for convention checking beacause it includes:")
+    print(*changed_files, sep="\n",)
+    sys.exit(1)
+
+
+@click.command()
+@click.option(
+    '-f', '--function',
+    type=str,
+    default=None,
+    help='Choose a function in git-ci'
+)
+@click.option(
+    '-c', '--commitsha',
+    type=str,
+    default=None,
+    help='Commit SHA'
+)
+def main(function, commitsha):
+    """Run main program"""
+    repo = Repo(path="./")
+    list_functions = {
+        "skip_convention_checking": skip_convention_checking,
+    }
+    list_functions[function](repo=repo, commit_hash=commitsha)
+
+
+if __name__ == "__main__":
+    main()
