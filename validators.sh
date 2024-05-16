ab735f142faccc7719f44659249678b055d53aa4
hungnd8 <hungnd8@vng.com.vn>
2024-05-16 14:32:17 +0700

    edit ci

--- validators.sh
+++ validators.sh
@@ -23,7 +23,7 @@ run_ci () {
     done
     modules=$(bazel query --noshow_progress --output package "set(${files[*]})" )
     if [[ ! -z ${modules} ]]; then
-        # make install
+        make install
         echo "---Check convention...---"
         python3 -m flake8 ${modules} --show-source --statistics && python3 -m pylint ${modules}
         if [ $? != 0 ]; then
